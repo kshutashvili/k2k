81c392aa1d823f09e762c3f5dd3afe7199bcba2e
Pavel Koryakin <xtr72@yandex.ru>
2016-10-04 13:31:41 +0300

    K2K-17 Make show draft flatpage to admins

--- info/views.py
+++ info/views.py
@@ -4,5 +4,9 @@ from info.models import Flatpage
 
 
 def flatpage(request, slug):
-    page = get_object_or_404(Flatpage, slug=slug, is_draft=False)
+    kwargs = {'slug': slug}
+    if not (request.user.is_authenticated()
+            and request.user.has_perm('info.change_flatpage')):
+        kwargs['is_draft'] = False
+    page = get_object_or_404(Flatpage, **kwargs)
     return render(request, 'info/flatpage.html', {'page': page})
