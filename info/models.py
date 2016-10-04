7a8ae7b1b20d744c1f4aa34b259e27f0f90cbd75
Pavel Koryakin <xtr72@yandex.ru>
2016-10-04 13:15:09 +0300

    K2K-17 Make order not unique

--- info/models.py
+++ info/models.py
@@ -27,13 +27,6 @@ class LandingTabManager(models.Manager):
         kwargs['content__is_draft'] = False
         return self.exclude(content=None).filter(**kwargs)
 
-    def free_tab(self):
-        curr = self.aggregate(models.Max('order'))['order__max']
-        try:
-            return curr + 1
-        except TypeError:
-            return 1
-
 
 class LandingTab(models.Model):
     class Meta:
@@ -46,8 +39,7 @@ class LandingTab(models.Model):
 
     objects = LandingTabManager()
 
-    order = models.IntegerField('Порядок', default=objects.free_tab,
-                                unique=True)
+    order = models.IntegerField('Порядок', default=0)
     content = models.OneToOneField(Flatpage, verbose_name='Страница',
                                    limit_choices_to={'is_draft': False},
                                    blank=True, null=True)
